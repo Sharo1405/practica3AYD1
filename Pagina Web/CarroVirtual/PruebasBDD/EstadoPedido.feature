82fbdb9fab156d76716e5cb50a92da424d8cf062
David Andres Alcazar Escobar <alcazarescobar2006@gmail.com>
2018-10-12 16:41:15 -0600

    Prueba BDD Modulo editar Pedido

---
+++ Pagina Web/CarroVirtual/PruebasBDD/EstadoPedido.feature
@@ -0,0 +1,13 @@
+﻿Feature: Modificar el estado de un pedido
+	Para poder modificar el estado de un pedido y evitar errores en su cambio
+	como usuario administrador
+	quiero que al seleccionar un pedido especifico se pueda cambiar el estado 
+	y muestre si el cambio fue correcto.
+
+@ModEstado
+Scenario: CambiarEstadoDeIniciadoACompletado
+	Given el codigo de la factura 1000
+	And el estado de la factura "Iniciado" con su identificador 1
+	And el nuevo estado de la factura "Completado" con su identificador 2
+	When al momento de presionar el boton de Editar Pedido
+	Then se debe realizar la modificacion del pedido y validar con un valor distinto de 0
---
+++ Pagina Web/CarroVirtual/PruebasBDD/ModificarElEstadoDeUnPedidoSteps.cs
@@ -0,0 +1,47 @@
+﻿using CarroVirtual;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TechTalk.SpecFlow;
+
+namespace PruebasBDD
+{
+    [Binding]
+    public class ModificarElEstadoDeUnPedidoSteps
+    {
+        private int IdFactura;
+        private int IdNuevoEstado;
+        private int IdAnteriorEstado;
+        private int resultado;
+        private ClassEditarPedido classEPedido = new ClassEditarPedido();
+
+        [Given(@"el codigo de la factura (.*)")]
+        public void GivenElCodigoDeLaFactura(int p0)
+        {
+            IdFactura = p0;
+        }
+
+        [Given(@"el estado de la factura ""(.*)"" con su identificador (.*)")]
+        public void GivenElEstadoDeLaFacturaConSuIdentificador(string p0, int p1)
+        {
+            IdAnteriorEstado = p1;
+        }
+
+        [Given(@"el nuevo estado de la factura ""(.*)"" con su identificador (.*)")]
+        public void GivenElNuevoEstadoDeLaFacturaConSuIdentificador(string p0, int p1)
+        {
+            IdNuevoEstado = p1;
+        }
+
+        [When(@"al momento de presionar el boton de Editar Pedido")]
+        public void WhenAlMomentoDePresionarElBotonDeEditarPedido()
+        {
+            resultado = ClassEditarPedido.editarPedido(IdFactura.ToString(), IdNuevoEstado.ToString());
+        }
+
+        [Then(@"se debe realizar la modificacion del pedido y validar con un valor distinto de (.*)")]
+        public void ThenSeDebeRealizarLaModificacionDelPedidoYValidarConUnValorDistintoDe(int p0)
+        {
+            Assert.AreNotEqual(p0, resultado);
+        }
+    }
+}
